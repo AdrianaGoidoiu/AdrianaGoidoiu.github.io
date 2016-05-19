4c3433b3add420e7b935264fc396a9964b839f62
Adriana Goidoiu <goidoiuadriana@gmail.com>
2016-05-19 19:44:42 +0100

    Update script.php

--- PHP/script.php
+++ PHP/script.php
@@ -1,14 +1,20 @@
 <?php
-$to="goidoiuadriana@gmail.com";
-$subject="Message from my webPage";
-$email=$_REQUEST['email'];
-$message=$_REQUEST['message'];
-$headers="From:$email";
-$sent=mail($to, $subject, $message, $headers);
-if($sent)
-{print('<p>"Thank you"</p>');
-}
-else {
-  print "error";
-}
+$myEmail="goidoiuadriana@gmail.com";
+$emailSubject="Message from my webPage";
+$firstName=$_POST['firstName'];
+$lastName=$_POST['lastName'];
+$emailFrom=$_POST['email'];
+$message=$_POST['message'];
+
+$body = <<<EOD
+<br> <hr> <br>
+From: $emailFrom <br>
+First Name: $firstName <br>
+Last Name: $lastName <br>
+Message: $message <br>
+EOD;
+
+$headers="From $email \r \n";
+$headers .="Content-type: text/html\r\n";
+$success=mail($myEmail, $emailSubject, $body, $headers);
 ?>
