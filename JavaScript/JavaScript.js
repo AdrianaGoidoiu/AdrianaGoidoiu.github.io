80f2d8bc05f55c733fe40819b99c9184c139962c
Adriana Goidoiu <goidoiuadriana@gmail.com>
2016-05-19 17:22:39 +0100

    Update JavaScript.js

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -3,7 +3,17 @@ var main=function(){
 	 	$('a').css("color","white");
 	 	$(this).css("color","red");
    	
-   	});	
+   	});
+   	$("form").submit(function(){
+   		var firstName=$("#FirstName").val();
+   		var lastName=$("#LastName").val();
+   		var email=$("#Email").val();
+   		var message=$("#Message").val();
+   		if(firstName ===""){
+   			 $(".first_name_error").text("Please enter your first name.");
+   		}
+   	})
+   	
 }
 
 
