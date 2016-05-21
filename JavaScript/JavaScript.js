85cd63cb803cd551364a15383beb52753c118c0f
Adriana Goidoiu <goidoiuadriana@gmail.com>
2016-05-21 20:29:08 +0100

    Contact working

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -2,29 +2,55 @@ var main=function(){
 	 $('a').click(function() {
 	 	$('a').css("color","white");
 	 	$(this).css("color","red");
-   	
    	});
+
    	$("form").submit(function(){
+                var values = $(this).serialize();
    		var firstName=$("#firstName").val();
    		var lastName=$("#lastName").val();
    		var email=$("#email").val();
    		var message=$("#message").val();
+                $('#firstName, #lastName, #email, #message').keypress(function(key){
+                   if(key.which){
+                         $(this).css("border-color","#ccc"); }
+                    });
+
    		if(firstName == ""){
-    			$(".first_name_error").text("I would like to know your first name");
-    		}
+    			document.getElementsByName('firstName')[0].placeholder="I would like to know your first name";
+                        $("#firstName").addClass('error');
+                }
    		if(lastName == ""){
-    			$(".last_name_error").text("I would like to know your last name");
+    			document.getElementsByName('lastName')[0].placeholder="I would like to know your last name";
+                        $("#lastName").addClass('error');
     		}
     		if(email == ""){
-    			$(".email_error").text("Please enter your email address");
+    			document.getElementsByName('email')[0].placeholder="Please enter your email address";
+                        $("#email").addClass('error');
     		}
-    		if(message === ""){
-    			$(".message_error").text("There is no point to send me an email without a message");
-    		}
-    		return false;
+    		if(message == ""){
+    			document.getElementsByName('message')[0].placeholder="There is no point to send me an email without a message";
+                        $("#message").addClass('error');
+  
+  		}
+                if(firstName!="" && lastName!="" && email!="" && message!=""){
+       $.ajax({
+        url: "../PHP/script.php",
+        type: "post",
+        data: values ,
+        success: function (response) {
+           $('input, textarea').val("");      
+           $(':input').removeAttr('placeholder');        
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+           console.log(textStatus, errorThrown);
+        }
+
+
+    });
+                }
+    		return false;            
    	});
-   	
-}
-
-
+   	}
 $(document).ready(main);
+						
--- PHP/script.php
+++ PHP/script.php
@@ -5,25 +5,15 @@ $firstName=$_POST['firstName'];
 $lastName=$_POST['lastName'];
 $emailFrom=$_POST['email'];
 $message=$_POST['message'];
+echo "Buna";
 
+if($firstName!="" && $lastName!="" && $emailFrom!="" && $message!=""){
 $body = <<<EOD
-<br> <hr> <br>
-From: $emailFrom <br>
-First Name: $firstName <br>
-Last Name: $lastName <br>
-Message: $message <br>
+From: $firstName $lastName 
+Contact: $emailFrom
+Message: $message
 EOD;
-
-$headers="From $email \r \n";
-$headers .="Content-type: text/html\r\n";
+$headers="From:" .$emailFrom;
 $success=mail($myEmail, $emailSubject, $body, $headers);
-$theResult = <<<EOD
-<html>
-<head></head>
-<body>
-<h1>AI REUSIT!</h1>
-</body>
-</html>
-EOD;
-echo "$theResults";
+}
 ?>
